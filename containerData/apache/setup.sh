349efd6ef7d4805dc8bb896648f36088e39b380d
h.pries <hp@hannespries.de>
2020-07-10 12:32:26 +0200

    fixes in docker-setup

--- containerData/apache/setup.sh
+++ containerData/apache/setup.sh
@@ -1,7 +1,7 @@
-FILE=/var/www/html/vendor/autoload.php
-if test -f "$FILE"; then
-    php /var/www/html/utils/composer.phar update
-else
-    php /var/www/html/utils/composer.phar install   
-fi
+#FILE=/var/www/html/vendor/autoload.php
+#if test -f "$FILE"; then
+#    php /var/www/html/utils/composer.phar update
+#else
+#    php /var/www/html/utils/composer.phar install
+#fi
 /usr/sbin/apache2ctl -D FOREGROUND
